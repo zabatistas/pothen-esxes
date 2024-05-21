2e9607a81f9a8f9cc21cd792cd657f2cbae5ca01
zabatistas <zabatistas@gmail.com>
2024-05-21 22:04:27 +0300

    Add Spring Boot project

---
+++ pothen-esxes/src/main/java/com/pothen/esxes/pothenesxes/PothenEsxesApplication.java
@@ -0,0 +1,13 @@
+package com.pothen.esxes.pothenesxes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PothenEsxesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PothenEsxesApplication.class, args);
+	}
+
+}
---
+++ pothen-esxes/src/test/java/com/pothen/esxes/pothenesxes/PothenEsxesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pothen.esxes.pothenesxes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PothenEsxesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
